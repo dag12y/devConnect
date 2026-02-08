085344ef3b2b79b63f8a3b64a53347d602c69464
dagm <dagimyibabe19@gmail.com>
2026-02-08 09:16:42 +0300

    feat: add uploadProfile function to handle user profile uploads and validations

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -1,17 +1,19 @@
 import Profile from "../models/Profile.js";
+import { validationResult } from "express-validator";
 
 export async function getProfile(req, res) {
     try {
-        const profile = await Profile.findOne({user:req.user.id}).populate('user',['name','avatar'])
-        if(!profile){
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            "user",
+            ["name", "avatar"],
+        );
+        if (!profile) {
             return res.status(400).json({
                 success: false,
                 errors: [{ msg: "There is no profile for this user" }],
             });
         }
-        return res.json({success: true,
-            data: profile
-        })
+        return res.json({ success: true, data: profile });
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({
@@ -19,4 +21,76 @@ export async function getProfile(req, res) {
             errors: [{ msg: "Server Error" }],
         });
     }
+}
+
+export async function uploadProfile(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            success: false,
+            errors: errors.array(),
+        });
+    }
+
+    const {
+        company,
+        website,
+        location,
+        status,
+        skills,
+        bio,
+        githubusername,
+        youtube,
+        twitter,
+        facebook,
+        linkedin,
+        instagram,
+    } = req.body;
+
+    //build profile object
+    const profileFields={}
+    profileFields.user  = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(status) profileFields.status = status;
+    if(bio) profileFields.bio = bio;
+    if(githubusername) profileFields.githubusername = githubusername;   
+    if(skills) {
+        profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+
+    //build social object
+    profileFields.social = {};
+    if(youtube) profileFields.social.youtube = youtube;
+    if(twitter) profileFields.social.twitter = twitter;
+    if(facebook) profileFields.social.facebook = facebook;
+    if(linkedin) profileFields.social.linkedin = linkedin;
+    if(instagram) profileFields.social.instagram = instagram;
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        if (profile) {
+            //update
+            const updatedProfile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true },
+            );
+            return res.json({ success: true, data: updatedProfile });
+        }
+        //create
+        const newProfile = new Profile(profileFields);
+        await newProfile.save();
+        return res.json({ success: true, data: newProfile });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({
+            success: false,
+            errors: [{ msg: "Server Error" }],
+        })
+        
+    }
+
 }
--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authMiddleware from '../../middleware/auth.middleware.js'
-import { getProfile } from "../../controllers/profile.controller.js";
+import { getProfile,uploadProfile } from "../../controllers/profile.controller.js";
+import { check } from "express-validator";
 
 const router = express.Router();
 
@@ -8,5 +9,15 @@ const router = express.Router();
 //@desc get current user profile 
 //@access Private
 router.get("/me", authMiddleware,getProfile);
+
+//@route Post api/profile
+//@desc upload new user profile
+//@access Private
+router.post("/",authMiddleware, [
+    check('status','Status is required').not().isEmpty(),
+    check("skills","Skills is required").not().isEmpty(),
+],uploadProfile);
+
+
 
 export default router;
